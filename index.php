3cb99fdfcd9e13d1fa3e179daa1917a0dd6dbcba
Sergey <sergius2109.ne@gmail.com>
2017-09-02 13:31:05 +0300

    echo key - delete / OK

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 // array_mixing
 
-$list = array(1, 2, 3, 4, 5, a, b, c, d, f,);
+$list = array(1, 2, 3, 4, 5, A, B, C, D, F,);
 
 $count = count($list);
 
 for ($i = 0; $i < $count/2; $i++) {
     $key = rand(0, $count-1);
-    echo $key;
     $tmp = $list[$key];
     $key2 = $count - $key - 1;
+    
     $list[$key] = $list[$key2];
     $list[$key2] = $tmp;
 }
