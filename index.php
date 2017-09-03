e7f1f9e22e3d2367581e0ca959c9301380add524
Sergey <sergius2109.ne@gmail.com>
2017-09-03 20:19:27 +0300

    OK array - FIX code - CLEARED

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 // array_mixing
 
-$list = array(1, 2, 3, 4, 5, 'A', 'B', 'C', 'D', 'F',);
+$list = array(1, 2, 3, 4, 5, 'A', 'B', 'C', 'D', 'F');
 
 $count = count($list);
 
 for ($i = 0; $i < $count/2; $i++) {
+    $tmp = $list[$i];
     $key = rand(0, $count-1);
-    $tmp = $list[$key];
-    $key2 = $count - $key - 1;
-    
-    $list[$key] = $list[$key2];
-    $list[$key2] = $tmp;
+            
+    $list[$i] = $list[$key];
+    $list[$key] = $tmp;
 }
 echo '<pre>';
 print_r($list);
