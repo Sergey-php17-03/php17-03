dfdf05c853074dadd823e73a907dec1f96e5d3f0
Sergey <sergius2109.ne@gmail.com>
2017-09-24 17:37:17 +0300

    table.php sort a-z - OK

--- table.php
+++ table.php
@@ -9,9 +9,7 @@
   примерно как должно работать -
   http://i.prntscr.com/nM8A9KnISjq2i9Ifc-4fzg.png
   http://js-grid.com/demos/
- */
 
-/*
   усложнение
   добавить поле сортировки и через array filter отсеивать лишние данные
 
@@ -25,50 +23,65 @@ $cars = array
     (
     array("Volvo", 22, 18),
     array("BMW", 15, 13),
-    array("Saab", 5, 2),
+    array("Saab", 5, 20),
     array("Land Rover", 17, 15)
 );
 
-
-if (isset($_GET['sort'])){
-$sort = $_GET['sort'];
+function flag($key) {
+    static $flag = [0, 0, 0,];
+    $flag[$key] ++;
+    return $flag;
 }
-$count = count($cars);
-$flag = function ($sort){
-    static $sort;
-    if (empty($sort)){
-        $method = 3;
-    }else{$method = $sort%2;}
-    $mnemo = array (
-        '&#9660',
-        '&#9650',
-        ' ');
-    return (string) $mnemo[$method];
+
+if (isset($_GET['key'])) {
+    $key = $_GET['key'];
+    $flag = flag($key);
+}
+
+$direction = [
+    '0' => '',
+    '1' => '&#9660',
+    '2' => '&#9650',
+];
+
+$rSorter = function ($a, $b) use ($key) {
+    return strnatcmp($b[$key], $a[$key]);
 };
+$sorter = function ($a, $b) use ($key) {
+    return strnatcmp($a[$key], $b[$key]);
+};
+
+usort($cars, $sorter);
+
 // generate table
+
+function column($data) {
+    echo "<td>$data</td>";
+}
+
 echo '<form action="/table.php" method="GET">
 <table border="1" width="500">
     <thead >
         <tr>
-            <th>'; echo $flag($sort); echo'<button name="sort" value="1">Auto</button></th>
-            <th>'; echo $flag($sort); echo'<button name="sort" value="1">Warranty Service</button></th>
-            <th>'; echo $flag($sort); echo'<button name="sort" value="1">Price (Million)</button></th>
+            <th>' . $direction[$flag[0]] . '<button name="key" value="0">Auto</button></th>
+            <th>' . $direction[$flag[1]] . '<button name="key" value="1">Warranty Service</button></th>
+            <th>' . $direction[$flag[2]] . '<button name="key" value="2">Price (Million)</button></th>
         </tr>
         <tr>
-            <th><input type="text" name="searchAuto" value=""/></th>
-            <th><input type="text" name="$searchWarrantyService" value=""/></th>
-            <th><input type="text" name="$searchPrice" value=""/></th>
+            <th><input type="text" name="search[Auto]" value=""/></th>
+            <th><input type="text" name="search[Warranty]" value=""/></th>
+            <th><input type="text" name="search[Price]" value=""/></th>
         </tr>
         
     </thead>
     <tbody >';
 
-foreach ($cars as $car) {
-    echo"<tr>"
-    . "<td>$car[0]</td>"
-    . "<td>$car[1]</td>"
-    . "<td>$car[2]</td>"
-    . "</tr>";
+foreach ($cars AS $item) {
+    echo '<tr>';
+    foreach ($item AS $data) {
+        column($data);
+    }
+    echo '</tr>';
 }
 
 echo'</tbody>
