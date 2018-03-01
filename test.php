bbe62259ec83da7950e86591027e4f5d352217d9
sergius2109 <31551632+Sergey-php17-03@users.noreply.github.com>
2018-03-01 21:23:40 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,10 @@ namespace testing;
 require_once __DIR__ .'/helloOOD.php';
     use \sandbox\test as TEst;
 
-$test1 = new TEst(First);
+$test1 = new TEst('First');
 echo '<br>';
 $test1->helloWorld();
 echo '<br>';
 $test1->iHave();
 echo '<br>';
-$test1->echoTextBold('Жирный?');
+$test1->echoTextBold('Этот текст должен быть жирным');
